fd1b2b52ba907439a8db2b931156673b39a6c8f7
PeachCoolpis <rlglxh@naver.com>
2025-12-17 03:39:20 +0900

    feat. 백엔드 ci/cd 세팅

---
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/log/JpaRepositoryMethodInterceptor.java
@@ -0,0 +1,28 @@
+package io.awportfoiioapi.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class JpaRepositoryMethodInterceptor {
+    
+    @Pointcut("execution(* io.awportfoiioapi..*.*(..))")
+    public void jpaRepositoryMethods() {}
+    
+    @Before("jpaRepositoryMethods()")
+    public void beforeQuery(JoinPoint joinPoint) {
+        String methodSignature = joinPoint.getSignature().toShortString();
+        JpaQueryContextHolder.set(methodSignature);
+    }
+    
+    @After("jpaRepositoryMethods()")
+    public void afterQuery() {
+        JpaQueryContextHolder.clear();
+    }
+}
