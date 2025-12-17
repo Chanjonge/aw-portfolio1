8210fc11c29f21b17b625f507adc4d4d96855b64
parkjs <park74441@naver.com>
2025-12-17 14:49:32 +0900

    [chore] useRequest 추가 및 서비스 폴더 추가

---
+++ aw-portfoiio-front/hooks/useRequest.ts
@@ -0,0 +1,34 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import axios, { AxiosError } from 'axios';
+
+interface RequestOptions {
+    ignoreErrorRedirect?: boolean;
+}
+
+export function useRequest() {
+    const router = useRouter();
+
+    const request = async <T>(
+        requestFn: () => Promise<T>,
+        onSuccess?: (data: T) => void,
+        options?: RequestOptions
+    ): Promise<T | undefined> => {
+        try {
+            const data = await requestFn();
+            onSuccess?.(data);
+            return data;
+        } catch (error) {
+            const err = error as AxiosError<any>;
+
+            if (options?.ignoreErrorRedirect) {
+                alert(err.response?.data?.message ?? '오류가 발생했습니다.')
+
+            }
+            throw error;
+        }
+    };
+
+    return { request };
+}
---
+++ aw-portfoiio-front/services/auth.service.ts
@@ -0,0 +1,24 @@
+// services/auth.service.ts
+import api from "@/lib/axiosInstance";
+
+export const AuthService = {
+    login: (email : string, password : string) =>
+        api.post('/auth/login', { email, password }),
+
+    me: () =>
+        api.get('/auth/me'),
+};
+
+// 호출단 예시
+// const handleLogin = async () => {
+//     await request(
+//         () => AuthService.login(email, password),
+//         (res) => {
+//             const { token, user } = res.data;
+//             localStorage.setItem('token', token);
+//             localStorage.setItem('user', JSON.stringify(user));
+//             router.push('/admin');
+//         },
+//         { ignoreErrorRedirect: true }
+//     );
+// };
