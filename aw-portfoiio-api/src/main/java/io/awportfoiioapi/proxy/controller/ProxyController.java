a43b1b3ef078bb6d3590630283cc299b768d2f54
배성환 <baeseongwhan@ulim.co>
2026-01-02 14:21:21 +0900

    feat. 프록시 컨트롤러 추가

---
+++ aw-portfoiio-api/src/main/java/io/awportfoiioapi/proxy/controller/ProxyController.java
@@ -0,0 +1,33 @@
+package io.awportfoiioapi.proxy.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProxyController {
+    
+    private final RestClient restClient;
+    
+    @GetMapping("/proxy")
+    public ResponseEntity<String> proxy(@RequestParam String url) {
+        // http만 허용
+        if (!url.startsWith("http://")) {
+            return ResponseEntity.badRequest().body("Only http allowed");
+        }
+
+        String body = restClient.get()
+                .uri(url)
+                .retrieve()
+                .body(String.class);
+
+        return ResponseEntity.ok(body);
+    }
+}
