27f0f001caead2f84d65bcd3d6789320619b3de5
parkjs <park74441@naver.com>
2026-01-02 11:33:17 +0900

    [fix] prisma 제거 후 테스트

--- aw-portfoiio-front/app/AuthListener.tsx
+++ aw-portfoiio-front/app/AuthListener.tsx
@@ -2,25 +2,10 @@
 import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import { userState } from "@/store/user";
-import axios from "axios";
 import { refreshToken } from "@/lib/axiosInstance";
 
 export default function AuthListener() {
   const setUser = useSetRecoilState(userState);
-
-  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-  // useEffect(() => {
-  //   const handler = (e: Event) => {
-  //     const custom = e as CustomEvent;
-  //     if (custom.detail?.user) {
-  //       setUser(custom.detail.user);
-  //     }
-  //   };
-  //
-  //   window.addEventListener("auth:refreshed", handler);
-  //   return () => window.removeEventListener("auth:refreshed", handler);
-  // }, [setUser]);
 
   useEffect(() => {
     const handler = (e: Event) => {
@@ -35,6 +20,7 @@ export default function AuthListener() {
     // 최초 마운트 시 refresh 한 번 시도
     refreshToken().catch(() => {
       // 비로그인 상태
+      console.log("비로그인 상태 체크");
     });
 
     return () => {
